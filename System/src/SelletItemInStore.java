706ec191d4be78559a6266c4ea4a46b5f5bb93a1
oronBortman <oron.bortman@gmail.com>
2020-08-03 14:13:12 +0300

    Adding classes

---
+++ System/src/SelletItemInStore.java
@@ -0,0 +1,2 @@
+public class SelletItemInStore {
+}
---
+++ UI/src/mainClass.java
@@ -0,0 +1,6 @@
+public class mainClass {
+    public static void main(String[] args) {
+        Menu m1 = new Menu();
+        m1.chooseOptionFromMainMenuUntilExit();
+    }
+}
